8a0f40c7459d531af8863fc93570fa26f5b4e1fd
gegub <g.bogusz93@gmail.com>
2015-05-27 17:01:41 +0000

    Akcje edycji formularzy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,36 @@
 class ArticlesController < ApplicationController
     
-     def index
+    def index
         @articles = Article.all
-     end
+    end
   
     def new
-        
+        @article = Article.new
     end 
     
     def create
         @article = Article.new(article_params)
-        
-        @article.save
-        redirect_to @article
+ 
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
+    end
+    
+    def edit
+        @article = Article.find(params[:id])
+    
+    end
+    
+    def update
+          @article = Article.find(params[:id])
+         
+          if @article.update(article_params)
+            redirect_to @article
+          else
+            render 'edit'
+          end
     end
     
     def show
